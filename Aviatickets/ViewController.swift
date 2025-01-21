12066a2284b1092a88d385a8e862b37ec9dc2b5d
yerimbet.aitzhanov <king_erosh@icloud.com>
2025-01-21 21:53:16 +0500

    Initial Commit

---
+++ Aviatickets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aviatickets
+//
+//  Created by Ерош Айтжанов on 21.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AviaticketsTests/AviaticketsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AviaticketsTests.swift
+//  AviaticketsTests
+//
+//  Created by Ерош Айтжанов on 21.01.2025.
+//
+
+import Testing
+@testable import Aviatickets
+
+struct AviaticketsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

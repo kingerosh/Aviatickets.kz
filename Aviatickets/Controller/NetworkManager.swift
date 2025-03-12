df7dd7312853aa55d0ff496de5bac8396a276bde
yerimbet.aitzhanov <king_erosh@icloud.com>
2025-03-12 08:48:14 +0500

    v 1.0

---
+++ Aviatickets/Controller/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Aviatickets
+//
+//  Created by Ерош Айтжанов on 15.02.2025.
+//
+
+import Foundation
---
+++ Aviatickets/View/SubClass/FlightTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FlightTableViewCell.swift
+//  Aviatickets
+//
+//  Created by Ерош Айтжанов on 20.02.2025.
+//
+
+import UIKit
+
+class FlightTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
